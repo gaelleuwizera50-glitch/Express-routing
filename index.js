d1e0fe41460b33f939a92c3b28fcf6a70217e9b6
gaelleuwizera50-glitch <gaelleuwizera50@gmail.com>
2026-04-29 20:54:11 +0300

    created a Mongodb storage

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const connectDB = require('./db');
+
+require("dotenv").config()
 
 const app = express();
 
@@ -67,9 +70,11 @@ app.post('/api/tasks', (req, res) => {
     app.use(express.json());
 
 })
-  const PORT = 3000; 
+  const PORT = process.env.PORT
 
   
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 })
+
+connectDB();
