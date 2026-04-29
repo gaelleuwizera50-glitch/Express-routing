a306a5e3c0b5ccc87a01b69b54f7664ef2e46a51
gaelleuwizera50-glitch <gaelleuwizera50@gmail.com>
2026-04-29 12:43:04 +0300

    did more backend

--- index.js
+++ index.js
@@ -2,37 +2,74 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request , response) =>{
-    response.send('This is the about page');
+// Middleware :Middleware are supposed to be positioned before the routes
+
+app.use((req, res, next ) => {
+    console.log (`${req.method} ${req.url}`);
+    next();
 })
-app.get('/profile', (request , response) =>{
-    response.send('This is the profile page');
+// In this case middleware is able ro read json request bodies
+app.use(express.json());
+
+// Get is the http method
+app.get('/', (request, response) => {
+    response.send('Welcome to my API');
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page')
+})
+
+
+// app.get('/profile', (request , response) =>{
+//     response.send('This is the profile page');
     
-});
+// });
 
 app.get('users/:id', (req, res) => {
     const userID = req.params.id;
     res.send(`User ID: ${userID}`);
 })
+
 app.get('/search', (req, res) => {
-    const query =req.query.keyword;
+    const query = req.query.keyword;
+
     res.send(`Search keyword: ${query}`);
 
     // http://localhost:3000/Search?keyword=Kampala.....what to type when searching in the engine
 })
-app.get("/api/products", (req,res) => {
+
+// Json responses
+app.get('/api/users', (req, res) => {
     const users = [
-        {id:1,name:'Alice'},
-        {id:2,name:'Bob'},
-        {id:3,name:'Charlie'}
+        {id: 1, name: 'Alice'},
+        {id: 2, name: 'Bob'},
+        {id: 3, name: 'Charlie'}
     ];
 res.json(users);
-
-
 });
-const PORT =3000;
 
+
+// POST http mtd
+app.post('/api/users', (req, res) => {
+const newUser = req.body;
+console.log('Received user data', newUser);
+
+})
+
+app.post('/api/tasks', (req, res) => {
+    const newTask = req.body;
+    console.log('Received user data:', newTask);
+    res.status(201).json({
+        message: 'Task created successfully',
+        user: newTask
+    })
+    app.use(express.json());
+
+})
+  const PORT = 3000; 
+
+  
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 })
-
